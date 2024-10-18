005e3f395f5e1ee3cfcf2f60626e123234f00fd0
pandajs <pandaJS404@gmail.com>
2024-10-18 14:31:59 +0800

    init-base

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,6 +7,7 @@ import { themeConfig } from "./config/theme";
 
 export default withMermaid(
   defineConfig({
+    base: "/docs/",
     lang: metaData.lang,
     title: metaData.title,
     description: metaData.description,
